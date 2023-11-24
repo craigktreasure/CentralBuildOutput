fbbecb3f4fe5d82c689bb2b0b13e35f0cc7d8cd5
Craig Treasure <craigktreasure@outlook.com>
2023-11-24 11:34:31 -0800

    Reusable workflows
    This change uses reusable workflows and actions to unify the workflows.

--- scripts/PatchGlobalJson.ps1
+++
@@ -1,9 +0,0 @@
-if ($true) {
-    return
-}
-
-Write-Host 'Disabling rollForward to pin to version in global.json.'
-$globalJsonPath = Join-Path $PSScriptRoot '../global.json'
-$globalJson = Get-Content $globalJsonPath -Raw | ConvertFrom-Json
-$globalJson.sdk.rollForward = 'disable'
-$globalJson | ConvertTo-Json | Set-Content -Path $globalJsonPath
